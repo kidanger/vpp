d102e83e44ad83ac586801acd2b20fee34920410
Jérémy Anger <angerj.dev@gmail.com>
2018-09-16 17:18:32 +0200

    add vpp_init_inputs

--- vpp.c
+++ vpp.c
@@ -1,29 +1,72 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h> // open, fnctl
+
+#include "vpp.h"
 
 #define VPP_TAG "VPP"
 
-FILE* vpp_init_input(const char* filename, int* w, int* h, int* d)
+static FILE* open_input(const char* filename)
+{
+    // open the file is non blocking mode in case this is a pipe
+    // we don't want to block on opening pipes since the other end
+    // will block until we open it (causing deadlocks with 2 pipes)
+    int fd = !strcmp(filename, "-") ? 0 : open(filename, O_RDONLY, O_NONBLOCK);
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
+
+    FILE* in = fdopen(fd, "rb");
+    setvbuf(in, NULL, _IONBF, 0);
+    return in;
+}
+
+static int read_header(FILE* in, int* w, int* h, int*d )
 {
     char tag[4];
-    FILE* in = !strcmp(filename, "-") ? stdin : fopen(filename, "rb");
-    setvbuf(in, NULL, _IONBF, 0);
-
-    if (!in)
-        goto err;
     if (!fread(tag, sizeof(tag), 1, in) || strncmp(tag, VPP_TAG, 4) != 0)
-        goto err;
+        return 0;
     if (   !fread(w, sizeof*w, 1, in)
         || !fread(h, sizeof*h, 1, in)
         || !fread(d, sizeof*d, 1, in))
-        goto err;
+        return 0;
+    return 1;
+}
 
-    return in;
-err:
-    if (in)
+FILE* vpp_init_input(const char* filename, int* w, int* h, int* d)
+{
+    FILE* in = open_input(filename);
+    if (in && !read_header(in, w, h, d)) {
         fclose(in);
-    return NULL;
+        return NULL;
+    }
+    return in;
+}
+
+int vpp_init_inputs(int n, FILE** files, const char** filenames, int* w, int* h, int* d)
+{
+    for (int i = 0; i < n; i++) {
+        files[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!(files[i] = open_input(filenames[i])))
+            goto err;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!read_header(files[i], w+i, h+i, d+i)) {
+            goto err;
+        }
+    }
+
+    return 1;
+err:
+    for (int i = 0; i < n; i++) {
+        if (files[i])
+            fclose(files[i]);
+    }
+    return 0;
 }
 
 FILE* vpp_init_output(const char* filename, int w, int h, int d)
--- vpp.h
+++ vpp.h
@@ -2,6 +2,7 @@
 #define VPP_HEADER
 
 FILE* vpp_init_input(const char* filename, int* w, int* h, int* d);
+int vpp_init_inputs(int n, FILE** files, const char** filenames, int* w, int* h, int* d);
 
 FILE* vpp_init_output(const char* filename, int w, int h, int d);
 
